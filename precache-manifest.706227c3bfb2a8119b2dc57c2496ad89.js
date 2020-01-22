a26760ec2519d73991ef19bfa4dc805320bdcd51
Brandon Fields <Brandon.F@abstract.com>
2020-01-21 23:09:09 -0600

    Updates

---
+++ precache-manifest.706227c3bfb2a8119b2dc57c2496ad89.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "119004971bb971bdde5e69f44e6a215b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1da7b628442d19a3107b",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "f14946afccb067e298f0",
+    "url": "/static/js/2.f4ae3405.chunk.js"
+  },
+  {
+    "revision": "b51e62edbcf7a9c4f4ad13f506021d3e",
+    "url": "/static/js/2.f4ae3405.chunk.js.LICENSE"
+  },
+  {
+    "revision": "1da7b628442d19a3107b",
+    "url": "/static/js/main.3e1171ce.chunk.js"
+  },
+  {
+    "revision": "90b31d4d845ec2cc1fea",
+    "url": "/static/js/runtime-main.c27bdd87.js"
+  }
+]);
